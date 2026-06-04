343d913576486bb18d04293e3c04f0d99a9e0ca2
v4ibh4v-p4nd3y <vaibhapanedy2427@gmail.com>
2026-06-05 00:31:46 +0530

    Create a program to generate fibonacci series

---
+++ DAY 4/13.c
@@ -0,0 +1,16 @@
+//Program to generate Fibonacci series 
+#include <stdio.h>
+
+#include <stdio.h>
+void main()
+{
+    int a=0,b=1,c,i;
+
+    for(i=0;i<=15;i++)
+    {
+        printf(" %d \n",a);
+        c=a+b;
+        a=b;
+        b=c;
+    }
+}
