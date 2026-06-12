73587a7458e467e0ab145b4815a8fab4284e439d
v4ibh4v-p4nd3y <vaibhapandey2427@gmail.com>
2026-06-12 16:49:49 +0530

    WEEK 2

---
+++ WEEK 2/DAY 8/29.c
@@ -0,0 +1,15 @@
+//Program to print half pyramid 
+#include <stdio.h>
+int main()
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
