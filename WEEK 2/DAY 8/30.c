309a2e2f6a50c36e53fa85b2889dcc7264fa0dac
v4ibh4v-p4nd3y <vaibhapandey2427@gmail.com>
2026-06-13 18:28:31 +0530

    Create a program to print number triangle

---
+++ WEEK 2/DAY 8/30.c
@@ -0,0 +1,14 @@
+//Program to print number triangle pattern
+#include <stdio.h>
+void main ()
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%d ",j);
+        }
+        printf("\n");
+    }
+}
