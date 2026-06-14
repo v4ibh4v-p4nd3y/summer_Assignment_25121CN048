0b13728843196afe3f634e8292bd117bf0f2b371
v4ibh4v-p4nd3y <vaibhapandey2427@gmail.com>
2026-06-14 07:40:54 +0530

    Create a program to print reverse star pyramid

--- WEEK 2/DAY 10/37.c
+++ WEEK 2/DAY 10/37.c
@@ -4,7 +4,7 @@ void main ()
 {
     int i, j, n=5;
     for (i = 1; i <= n; ++i) {
-        for (j = 1; j <= n - i; ++j) {
+        for (j = 1; j <= n- i; ++j) {
             printf("  ");
         }
         for (j = 1; j <= 2 * i - 1; ++j) {
