0f47d7144c7216e9c0be7a3ddea72c36f9819c19
v4ibh4v-p4nd3y <vaibhapandey2427@gmail.com>
2026-06-21 23:00:12 +0530

    Create a program to Convert lowercase to uppercase

---
+++ WEEK 3/DAY 21/84.c
@@ -0,0 +1,15 @@
+//Program to Convert lowercase to uppercase
+#include <stdio.h>
+void main(){
+    char str[100];
+    int i=0;
+    printf("Enter a string: ");
+    fgets(str,sizeof(str),stdin);
+    while (str[i]!='\0'){
+        if (str[i]>='a'&&str[i]<='z') {
+            str[i]=str[i]-32;   // Convert to uppercase
+        }
+        i++;
+    }
+    printf("Uppercase string: %s",str);
+}
