e96cc6183045ed3dd9d3d56573a1e90b5307c075
v4ibh4v-p4nd3y <vaibhapandey2427@gmail.com>
2026-06-21 21:52:08 +0530

    Create a program to subtract matrices

---
+++ WEEK 3/DAY 19/74.c
@@ -0,0 +1,34 @@
+//Write a program to Subtract matrices.
+#include <stdio.h>
+void main(){
+    int n,m,i,j;
+    printf("Enter the numbers of rows and columns of the matrix:");
+    scanf("%d%d",&n,&m);
+    int a[n][m],b[n][m],c[n][m];
+    printf("Enter the elements of first array:\n");
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("Enter the elements of the second array:");
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            scanf("%d",&b[i][j]);
+        }
+    }
+    // Subtracting the matrices
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            c[i][j] = a[i][j] - b[i][j];
+        }
+    }
+    // Displaying the result
+    printf("The difference of the matrices is:\n");
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            printf("%d ",c[i][j]);
+        }
+        printf("\n");
+    }
+}
