7ad2ce57a1be35c72757ad45040dd792e5a69aa5
v4ibh4v-p4nd3y <vaibhapandey2427@gmail.com>
2026-06-21 20:33:24 +0530

    Create a program to selection sort

---
+++ WEEK 3/DAY 18/70.c
@@ -0,0 +1,27 @@
+//Write a program to Selection sort. 
+#include <stdio.h>
+int main(){
+    int i,j,k,n,temp=0;
+    printf("enter no of elements of array : ");
+    scanf("%d", &n);
+    int a[n];
+    for(i=0;i<n;i++){
+        printf("enter element %d : ", i+1);
+        scanf("%d", &a[i]);
+    }
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            if(a[i]<a[j]){
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    //printing sorted array
+    printf("Sorted array : ");
+    for(i=0;i<n;i++){
+        printf("%d ", a[i]);
+    }
+    return 0;
+}
