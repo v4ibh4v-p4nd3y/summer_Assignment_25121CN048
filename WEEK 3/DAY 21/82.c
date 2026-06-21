97279e42fccc8addd79c070dbbe2fec71819199a
v4ibh4v-p4nd3y <vaibhapandey2427@gmail.com>
2026-06-21 22:53:11 +0530

    Create a program to Reverse a string

---
+++ WEEK 3/DAY 21/82.c
@@ -0,0 +1,19 @@
+//Write a program to Reverse a string
+#include <stdio.h>
+void main() {
+    char str[100],temp;
+    int i,l=0;
+    printf("Enter a string: ");
+    fgets(str,sizeof(str),stdin);
+    // Find string length
+    while (str[l]!='\0'&&str[l]!='\n'){
+        l++;
+    }
+    // Reverse the string
+    for (i=0;i<l/2;i++) {
+        temp=str[i];
+        str[i]=str[l-1-i];
+        str[l-1-i] =temp;
+    }
+    printf("Reversed string: %.*s\n",l,str);
+}
